a112df93c38542e8c3f718ac1a51b2c6790cbea1
nick neessen <nneessen@gmail.com>
2023-08-08 17:23:11 -0500

    a

--- if_else/main.py
+++ if_else/main.py
@@ -32,9 +32,7 @@
 year = int(input("What year do you want to check?\n"))
 
 if year % 4 == 0:
-    if year % 100 != 0:
-        print("Leap year.")
-    elif year % 400 == 0:
+    if year % 100 != 0 or year % 400 == 0:
         print("Leap year.")
     else:
         print("Not leap year.")
